0de02eade6e90c434478cc73bbc4e6079bdddf0c
indexerror <lkycst@gmail.com>
2023-07-12 22:59:39 +0700

    chatgpt refactor

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,44 +14,74 @@ interface CategoryEntry {
 
 const categoryList = ['Analysis', 'Highlights', 'Interviews', 'Predictions']
 
-export default ({ articles }: NavbarProps) => {
-  const [categories, setCategories] = useState<Record<string, CategoryEntry[]>>();
+export default function Navbar({ articles }: NavbarProps) {
+  const [categories, setCategories] = useState<Record<string, CategoryEntry[]>>({});
   const [highlightedTab, setHighlightedTab] = useState<string>();
 
-  // Build the list of articles per categories
-  useEffect(() => {
+  const buildCategories = () => {
     const newCategories: Record<string, CategoryEntry[]> = {};
-    Object.keys(articles).forEach(path => {
+
+    Object.keys(articles).forEach((path) => {
       const [category] = path.split('/');
-      if (!newCategories[category]) newCategories[category] = [];
-      if (articles[path].attributes.postName !== 'sample') {
-        newCategories[category].push({
-          title: articles[path].attributes.title,
-          href: `${import.meta.env.BASE_URL}${path}`,
-        });
+      if (!newCategories[category]) {
+        newCategories[category] = [];
+      }
+
+      const { postName, title } = articles[path].attributes;
+      if (postName !== 'sample') {
+        const href = `${import.meta.env.BASE_URL}${path}`;
+        newCategories[category].push({ title, href });
       }
     });
+
     setCategories(newCategories);
+  };
+
+  useEffect(() => {
+    buildCategories();
   }, []);
 
-  return <div className={classes.root} onMouseLeave={() => setHighlightedTab('')}>
-    <div className={classes.logo}>
-      <a href={import.meta.env.BASE_URL}><img alt="" src={logo} /></a>
+  const handleTabMouseOver = (category: string) => {
+    setHighlightedTab(category.toLowerCase());
+  };
+
+  const renderCategory = (category: string) => {
+    return (
+      <a
+        key={category}
+        className={classes.category}
+        onMouseOver={() => handleTabMouseOver(category)}
+      >
+        {category}
+      </a>
+    );
+  };
+
+  const renderEntry = (entry: CategoryEntry) => {
+    return (
+      <li key={entry.href} className={classes.entry}>
+        <a href={entry.href}>{entry.title}</a>
+      </li>
+    );
+  };
+
+  return (
+    <div className={classes.root} onMouseLeave={() => setHighlightedTab('')}>
+      <div className={classes.logo}>
+        <a href={import.meta.env.BASE_URL}>
+          <img alt="" src={logo} />
+        </a>
+      </div>
+      <div className={classes.categories}>
+        {Object.keys(categories).map(renderCategory)}
+      </div>
+      {highlightedTab && categories && (
+        <div className={classes.panelContainer}>
+          <ul className={classes.panel}>
+            {categories[highlightedTab].map(renderEntry)}
+          </ul>
+        </div>
+      )}
     </div>
-    <div className={classes.categories}>
-      {
-        categoryList.map(
-          (category: string) => <a className={classes.category} onMouseOver={
-            () => setHighlightedTab(category.toLowerCase())}>{category}</a>
-        )
-      }
-    </div>
-    {highlightedTab && categories && <div className={classes.panelContainer}>
-      <ul className={classes.panel}>
-        {categories[highlightedTab].map(entry => <li className={classes.entry}>
-          <a href={entry.href}>{entry.title}</a>
-        </li>)}
-      </ul>
-    </div>}
-  </div>;
-};
+  );
+}
