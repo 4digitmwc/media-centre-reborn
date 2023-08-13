d49f83154befede232eddb2b0f8b3f5a0ca1464c
indexerror <lkycst@gmail.com>
2023-08-13 13:28:43 +0800

    e -> _ now it works

--- src/components/Churros/Churros.tsx
+++ src/components/Churros/Churros.tsx
@@ -40,7 +40,7 @@ export const Churros: React.FC = () => {
         <Autocomplete 
             id="country" 
             options={countries} 
-            onChange={(e: any, newValue: string | null) => { setCountry(newValue || "Brazil"); e = e }} 
+            onChange={(_: any, newValue: string | null) => { setCountry(newValue || "Brazil") }} 
             renderInput={params => <TextField {...params} label="Country" />} 
         />
         {/* Roster Box */}
